4721b0619c42d84cdeb207b7941417692fe0067c
Ryohei Fujihara <giver.yell@gmail.com>
2024-06-16 08:22:01 +0900

    ♻️ Main.tsx

--- study-react/components/Headline.tsx
+++ study-react/components/Headline.tsx
@@ -3,14 +3,14 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 type HeadlineProps = {
-  pages: string;
+  page: string;
 };
 
-export function Headline({ pages }: HeadlineProps) {
+export function Headline({ page }: HeadlineProps) {
   return (
     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
     Get started by editing&nbsp;
-    <code className="font-mono font-bold">pages/{pages}.tsx</code>
+    <code className="font-mono font-bold">pages/{page}.tsx</code>
     </p>
   );
 }
