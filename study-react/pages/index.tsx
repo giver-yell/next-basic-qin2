0bdb5def88bcfa45e8a325855c9578034275337b
Ryohei Fujihara <giver.yell@gmail.com>
2024-06-17 07:36:31 +0900

    ✅ useEffect

--- study-react/pages/index.tsx
+++ study-react/pages/index.tsx
@@ -2,10 +2,21 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    console.log("mounted")
+    document.documentElement.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("unmounted")
+      document.documentElement.style.backgroundColor = "";
+    }
+  }, []);
+
   return (
     <>
       <Head>
