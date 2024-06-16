9999c39add53e9d86d68a96421fbb29ecbdf87af
Ryohei Fujihara <giver.yell@gmail.com>
2024-06-17 07:52:50 +0900

    ✅ useState

--- study-react/pages/index.tsx
+++ study-react/pages/index.tsx
@@ -2,17 +2,24 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [count, setCount] = useState(10);
+
+  const handleClick = () => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  };
+
   useEffect(() => {
-    console.log("mounted")
+    // console.log("mounted")
     document.documentElement.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log("unmounted")
+      // console.log("unmounted")
       document.documentElement.style.backgroundColor = "";
     }
   }, []);
@@ -23,6 +30,8 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
     </>
   );
