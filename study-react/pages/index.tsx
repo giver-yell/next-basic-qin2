c4663440269215bf3bad70132ee672c27e8777eb
Ryohei Fujihara <giver.yell@gmail.com>
2024-06-18 09:02:48 +0900

    ✨ useState2

--- study-react/pages/index.tsx
+++ study-react/pages/index.tsx
@@ -8,6 +8,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   // const handleClick = () => {
   //   setCount(count => count + 1);
@@ -19,6 +21,18 @@ export default function Home() {
       setCount(count => count + 1);
     }
   }, [count])
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内で入力してください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, [])
+
+  const handleDisplay = useCallback(() => {
+    setIsShow(isShow => !isShow);
+  }, [])
 
   // 第二引数が変更されるたびに実行される
   useEffect(() => {
@@ -37,8 +51,16 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+      />
       <Main page="index" />
     </>
   );
