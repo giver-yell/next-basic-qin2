77c65486d0f25b4b255eaff7b27bcabf3a9c4bc2
Ryohei Fujihara <giver.yell@gmail.com>
2024-06-25 07:18:55 +0900

    🎨 useSWR

--- components/Posts/index.tsx
+++ components/Posts/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import { useCallback, useEffect, useReducer } from "react";
+import useSWR from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,71 +22,32 @@ const initialState: State = {
   error: null,
 };
 
-type Action =
-  | { type: "FETCHING" }
-  | { type: "FETCHED"; payload: Post[] }
-  | { type: "ERROR"; payload: string }
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case "FETCHED":
-      return {
-        ...state,
-        data: action.payload,
-        loading: false,
-      };
-    case "ERROR":
-      return {
-        ...state,
-        error: action.payload,
-      };
-    default:
-      throw new Error("An error occurred while fetching the data");
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the data.");
   }
+  return response.json();
 }
 
 export const Posts = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, error, isLoading } = useSWR<Post[]>("https://jsonplaceholder.typicode.com/posts", fetcher);
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("An error occurred while fetching the data");
-      }
-      const json: Post[] = await res.json();
-      dispatch({ type: "FETCHED", payload: json });
-    } catch (err) {
-      if (err instanceof Error) {
-        dispatch({ type: "ERROR", payload: err.message });
-      } else {
-        dispatch({ type: "ERROR", payload: "An error occurred while fetching the data" });
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }
-  , [getPosts]);
-
-  console.log("bar");
-
-  if (state.loading) {
+  if (isLoading) {
     return <p>loading...</p>;
   }
 
-  if (state.error) {
-    return <p>{state.error}</p>;
+  if (error) {
+    return <p>{error.message}</p>;
   }
 
-  if (state.data.length === 0) {
+  if (!data || data.length === 0) {
     return <p>no posts found</p>;
   }
 
   return (
     <ol>
-      {state.data.map((post) => {
+      {data.map((post) => {
         return <li key={post.id}>{post.title}</li>
       })}
     </ol>
